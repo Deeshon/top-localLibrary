69117fcce5d1b54131db6decf9f983bce460f4c5
Deeshon Hunukumbura <deeshonhunukumbura18@gmail.com>
2023-04-09 23:11:48 +0530

    genre delete completed

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -102,15 +102,33 @@ exports.genre_create_post = [
   },
 ];
 
-
 // Display Genre delete form on GET.
-exports.genre_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+exports.genre_delete_get = function (req, res, next) {
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+      genre_books: function (callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      // Successful, so render.
+      res.render("genre_delete", {
+        title: "Delete Genre",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  Genre.findByIdAndRemove(req.body.id, (err) => {
+    res.redirect("/catalog/genres")
+  })
 };
 
 // Display Genre update form on GET.
